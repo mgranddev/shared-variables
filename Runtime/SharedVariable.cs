9934eb97036cd6271b6aea9c0c492b2a5e9b7425
Michael Grand <214573168+mgranddev@users.noreply.github.com>
2025-10-23 15:52:58 -0700

    Removed outdated TODO

--- Runtime/SharedVariable.cs
+++ Runtime/SharedVariable.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace MGrand.SharedVariables
 {
-    // TODO: This should be in a "Variables" folder, maybe with its own assembly.
     public abstract class SharedVariable<TValue> : ScriptableObject
     {
         [field: NonSerialized]
